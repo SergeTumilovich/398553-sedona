d7a35304c3a134e05f21498751079fb0dd1c41a2
SergeTumilovich <30292087+SergeTumilovich@users.noreply.github.com>
2017-09-08 10:24:53 +0300

    Минификация
    Добавлены и подключены минифицированные файлы style & script

---
+++ js/script-min.js
@@ -0,0 +1,1 @@
+var link=document.querySelector(".btn-search-hotel");var form=document.querySelector(".modal-search");link.addEventListener("click",function(evt){evt.preventDefault();form.classList.toggle("form-closed")})
